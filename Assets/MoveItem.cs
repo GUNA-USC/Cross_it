866a5f34656401e62b9ccd9a646c3172d094e9c7
Joey <joey_lian@163.com>
2018-11-20 17:40:04 -0800

    add moving obstacle and sky

---
+++ Assets/MoveItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveItem : MonoBehaviour
+{
+    int num = 1;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -1.8)
+        {
+            num = 1;
+        }
+        if (transform.position.x > 1.8)
+        {
+            num = -1;
+        }
+        transform.Translate(Vector3.right * Time.deltaTime * num);
+    }
+}
